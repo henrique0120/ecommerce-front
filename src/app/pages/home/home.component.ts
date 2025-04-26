4063ba90fbedb54e0473277f7dc50236193a3cdc
henrique0120 <henrique.sl959@hotmail.com>
2025-04-25 22:13:18 -0300

    cards

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { MenubarComponent } from "../../components/menubar/menubar.component";
 import { BannerComponent } from "../../components/banner/banner.component";
+import { ProductCardComponent } from '../../components/product-card/product-card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, MenubarComponent, BannerComponent],
+  imports: [NavbarComponent, MenubarComponent, BannerComponent, ProductCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
