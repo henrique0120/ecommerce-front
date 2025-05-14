1fe8e7c7d62962cb777750ff20205d2f0cc51925
henrique0120 <henrique.sl959@hotmail.com>
2025-05-14 12:51:52 -0300

    product component

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [MatInputModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
+open = false;
+toggle() {
+  this.open = !this.open;
+}
 
 }
